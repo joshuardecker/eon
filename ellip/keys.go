d8a2a84820a3945a728a140762f2d67db5a46d8e
Sucks-To-Suck <jr.plainzainey@gmail.com>
2022-07-18 01:00:13 -0600

    Added random message creation and signing, as well as signature verification

--- ellip/keys.go
+++ ellip/keys.go
@@ -5,6 +5,7 @@ import (
 	"crypto/elliptic"
 	"crypto/rand"
 	"encoding/hex"
+	"io"
 	"os"
 
 	"github.com/ethereum/go-ethereum/crypto"
@@ -55,10 +56,10 @@ func generatePrivateKey() {
 }
 
 // Gets the private key and generates the public key. Returns the public and private key.
-func GetKeyPair() (publicKey, privateKey []byte) {
+func GetKeyPair() (publicKey []byte, privateKey *ecdsa.PrivateKey) {
 
 	// Load the private key
-	key, readErr := crypto.LoadECDSA("saves/key")
+	privateKey, readErr := crypto.LoadECDSA("saves/key")
 
 	// If there was an error
 	if readErr != nil {
@@ -69,7 +70,7 @@ func GetKeyPair() (publicKey, privateKey []byte) {
 			generatePrivateKey()
 
 			// Loads the newly generated private key
-			key, readErr = crypto.LoadECDSA("saves/key")
+			privateKey, readErr = crypto.LoadECDSA("saves/key")
 
 			// If an error occured loading the private key
 			if readErr != nil {
@@ -85,14 +86,14 @@ func GetKeyPair() (publicKey, privateKey []byte) {
 	}
 
 	// Get the public key
-	publicKey = elliptic.Marshal(crypto.S256(), key.X, key.Y)
+	publicKey = elliptic.Marshal(crypto.S256(), privateKey.X, privateKey.Y)
 
 	// Define the private key
-	privateKey = make([]byte, 32)
+	//privateKey = make([]byte, 32)
 
 	// Cool binary stuff that works
-	binaryBlob := key.D.Bytes()
-	copy(privateKey[32-len(binaryBlob):], binaryBlob)
+	//binaryBlob := key.D.Bytes()
+	//copy(privateKey[32-len(binaryBlob):], binaryBlob)
 
 	return publicKey, privateKey
 }
@@ -110,4 +111,53 @@ func PubKeyHashStr() string {
 	sha3.ShakeSum256(publicKeyHash, publicKey)
 
 	return hex.EncodeToString(publicKeyHash)
+}
+
+// Gives a random message with only a size (bytes returned) input needed. Returns a byte array of the random message.
+func randomMessage(arraySize uint) []byte {
+
+	// Creates the byte buffer
+	bytesBuffer := make([]byte, arraySize)
+
+	// Read a random place for the message
+	_, readErr := io.ReadFull(rand.Reader, bytesBuffer)
+
+	// Not read correctly?
+	if readErr != nil {
+
+		panic(readErr)
+	}
+
+	return bytesBuffer
+}
+
+func SignRandMsg() (msgHash, sig []byte) {
+
+	_, privateKey := GetKeyPair()
+
+	msg := randomMessage(32)
+
+	msgHash = make([]byte, 32)
+	sha3.ShakeSum256(msgHash, msg)
+
+	sig, sigErr := crypto.Sign(msgHash, privateKey)
+
+	if sigErr != nil {
+
+		panic(sigErr)
+	}
+
+	finalSig := make([]byte, 64)
+
+	for index := 0; index < 64; index++ {
+
+		finalSig[index] = sig[index]
+	}
+
+	return msgHash, finalSig
+}
+
+func ValidateSig(publicKey, msgHash, sig []byte) bool {
+
+	return crypto.VerifySignature(publicKey, msgHash, sig)
 }
--- main/main.go
+++ main/main.go
@@ -1,10 +1,31 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/Sucks-To-Suck/LuncheonNetwork/blockchain"
+	"github.com/Sucks-To-Suck/LuncheonNetwork/ellip"
 )
 
 func main() {
+
+	fmt.Println("*****")
+	//****
+	// Test new features area
+
+	msg, sig := ellip.SignRandMsg()
+	pub, _ := ellip.GetKeyPair()
+
+	if ellip.ValidateSig(pub, msg, sig) == true {
+
+		fmt.Println("Worked!")
+	} else {
+		fmt.Println("Didnt work")
+	}
+
+	// Test new features area
+	//****
+	fmt.Println("*****")
 
 	//****
 	// Starts Mining
