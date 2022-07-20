79876edffb39025f34f5c9eb96e772af717bd36e
Sucks-To-Suck <jr.plainzainey@gmail.com>
2022-07-20 17:18:32 -0600

    Minor final changes

--- transactions/LuTx.go
+++ transactions/LuTx.go
@@ -1,11 +1,35 @@
 package transactions
 
+// This struct are the tx's on the Luncheon Network.
 type LuTx struct {
-	inScripts []txScript
+	inScripts []scriptStr
 
-	outScripts []txScript
+	outScripts []scriptStr
 }
 
-type txScript struct {
-	scriptStr string
+// This struct is the transaction script or scripts
+type scriptStr struct {
+	ScriptStr string
+}
+
+// This function adds a scriptStr to the tx scriptStrs.
+// First input is the scriptStr thats being added.
+// The second is a bool.
+// Enter true to add the scriptStr to the inScripts on the tx.
+// Enter false to add the scriptStr to the outScripts on the tx.
+// Returns nothing.
+func (l LuTx) AddScriptStr(scriptstr string, scriptType bool) {
+
+	tScript := new(scriptStr)
+	tScript.ScriptStr = scriptstr
+
+	if scriptType {
+
+		l.inScripts = append(l.inScripts, *tScript)
+	}
+
+	if !scriptType {
+
+		l.outScripts = append(l.outScripts, *tScript)
+	}
 }
