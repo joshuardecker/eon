8aff472a6234211152e1b27679fd7474089e6b41
Sucks-To-Suck <jr.plainzainey@gmail.com>
2022-07-21 01:38:51 -0600

    Added transaction flag checking to the mempool

--- blockchain/mempool.go
+++ blockchain/mempool.go
@@ -1,9 +1,11 @@
 package blockchain
 
-import "github.com/Sucks-To-Suck/LuncheonNetwork/transactions"
+import (
+	"github.com/Sucks-To-Suck/LuncheonNetwork/transactions"
+)
 
 type Mempool struct {
-	txs []transactions.LuTx
+	Txs []transactions.LuTx
 }
 
 // Function adds a tx to the mempool of the blockchain.
@@ -11,5 +13,130 @@ type Mempool struct {
 // Returns nothing.
 func (m *Mempool) AddTx(tx transactions.LuTx) {
 
-	m.txs = append(m.txs, tx)
+	m.Txs = append(m.Txs, tx)
+}
+
+// This function removes a tx from the mempool.
+// Returns nothing.
+func (m *Mempool) RemoveTx(index int) {
+
+	m.Txs = append(m.Txs[:index], m.Txs[index+1:]...)
+}
+
+func (m *Mempool) GetTx() any {
+
+	leng := len(m.Txs)
+
+	if leng == 0 {
+
+		return false
+	}
+
+	for index := 0; index < leng; index += 1 {
+
+		if m.CheckTxFlags(index) {
+
+			tx := m.Txs[index]
+			m.RemoveTx(index)
+
+			return tx
+		} else {
+
+			m.RemoveTx(index)
+
+			// So when it loops, it will be the same index value
+			index -= 1
+		}
+	}
+
+	return false
+}
+
+// This function checks whether a transaction has the correct flags or not.
+// Returns a bool if it is valid, true if yes, false if no.
+func (m *Mempool) CheckTxFlags(txIndex int) bool {
+
+	// Save a copy of the tx
+	tx := m.Txs[txIndex]
+
+	// Define what we are looking for
+	var keyWords []string = []string{
+		"TXID",     // Input
+		"OUTINDEX", // Input
+		"PUBK",     // Output
+		"SIG",      // Output
+		"PUBKH",    // Output
+		"AMT",      // Output
+		"SELF",     // Output, but later check
+	}
+
+	//****
+	// Check the input scripts
+
+	for index := 0; index < len(tx.InScripts); index += 1 {
+
+		// Parse the input scriptstr into a script
+		inputScript := transactions.ScriptParse(tx.InScripts[index].ScriptStr)
+
+		// Check keyword, starting at index 0, aka "TXID"
+		keyWordIndex := 0
+
+		// Check through the whole scipt
+		for nIndex := 0; nIndex < len(inputScript); nIndex += 1 {
+
+			// If the correct word was found
+			if inputScript[nIndex] == keyWords[keyWordIndex] {
+
+				keyWordIndex += 1
+
+				// When loops again, it will equal 0
+				nIndex = -1
+			}
+		}
+
+		// Since we were looking for checking for 2 input flags, it must equal 2
+		if keyWordIndex != 2 {
+
+			return false
+		}
+	}
+
+	// Check the input scripts
+	//****
+
+	//****
+	// Check output scripts
+
+	for index := 0; index < len(tx.OutScripts); index += 1 {
+
+		// Parse the input scriptstr into a script
+		outScript := transactions.ScriptParse(tx.OutScripts[index].ScriptStr)
+
+		// Check keyword, starting at index 2, aka "PUBK"
+		keyWordIndex := 2
+
+		// Check through the whole scipt
+		for nIndex := 0; nIndex < len(outScript); nIndex += 1 {
+
+			// If the correct word was found
+			if outScript[nIndex] == keyWords[keyWordIndex] {
+
+				keyWordIndex += 1
+
+				// When loops again, it will equal 0
+				nIndex = -1
+			}
+		}
+
+		// Since we were looking for checking for 4 input flags, it must equal 6 (4 + the starting value 2 = 6)
+		if keyWordIndex != 6 {
+
+			return false
+		}
+	}
+
+	// Check output scripts
+	//****
+
+	return true
 }
---
+++ blockchain/mempool_test.go
@@ -0,0 +1,40 @@
+package blockchain
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/Sucks-To-Suck/LuncheonNetwork/transactions"
+)
+
+func TestMempoolCheck(t *testing.T) {
+
+	tx1 := new(transactions.LuTx) // Valid
+	tx2 := new(transactions.LuTx) // Invalid
+	tx3 := new(transactions.LuTx) // Invalid, but trickier
+	tx4 := new(transactions.LuTx) // Valid, but trickier
+
+	tx1.AddScriptStr("TXID 123 OUTINDEX 456", true)
+	tx1.AddScriptStr("PUBK 123 SIG 456 PUBKH 789 AMT 50", false)
+
+	tx2.AddScriptStr("TXID 123", true)
+	tx2.AddScriptStr("PUBK 123", false)
+
+	tx3.AddScriptStr("TXID 123 OUTINDEX 456", true)
+	tx3.AddScriptStr("PUBK 123", false)
+
+	tx4.AddScriptStr("OUTINDEX 456 TXID 123 COOL COOL", true)
+	tx3.AddScriptStr("SIG 456 PUBK 123 TEST PUBKH 789 AMT 50", false)
+
+	m := new(Mempool)
+
+	m.AddTx(*tx1)
+	m.AddTx(*tx2)
+	m.AddTx(*tx3)
+	m.AddTx(*tx4)
+
+	fmt.Println("tx1:", m.CheckTxFlags(0)) // Should print true
+	fmt.Println("tx2:", m.CheckTxFlags(1)) // Should print false
+	fmt.Println("tx3:", m.CheckTxFlags(2)) // Should print false
+	fmt.Println("tx4:", m.CheckTxFlags(3)) // Should print true
+}
