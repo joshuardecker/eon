10e12dca6a55666cf89244ab38ca5293e41b246f
Sucks-To-Suck <jr.plainzainey@gmail.com>
2022-07-23 20:51:42 -0600

    Added the createTx func to the wallet

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -1,7 +1,11 @@
 package wallet
 
 import (
+	"encoding/hex"
+	"encoding/json"
+
 	"github.com/Sucks-To-Suck/LuncheonNetwork/blockchain"
+	"github.com/Sucks-To-Suck/LuncheonNetwork/ellip"
 	"github.com/Sucks-To-Suck/LuncheonNetwork/transactions"
 )
 
@@ -11,6 +15,7 @@ var SoftwareVersion string = "v1"
 type Wallet struct {
 	chain   *blockchain.Blockchain
 	version string
+	mainKey ellip.MainKey
 }
 
 func Init(b *blockchain.Blockchain) Wallet {
@@ -49,6 +54,26 @@ func (w *Wallet) ScanChainForBalance(pubKey string) (balance uint64) {
 	return balance
 }
 
+// Scans the blockchain for the available balance of a publicKey.
+// Returns the balance of the publicKey.
+func (w *Wallet) ScanChainForNonce(pubKey string) (nonce uint32) {
+
+	// Scans the blockchain, starting from the newest block to the first
+	for index := 0; index < len(w.chain.Blocks); index += 1 {
+
+		// Check each tx in the block
+		for txIndex := 0; txIndex < len(w.chain.Blocks[index].Txs); txIndex += 1 {
+
+			if w.chain.Blocks[index].Txs[txIndex].TxFrom == pubKey {
+
+				nonce += 1
+			}
+		}
+	}
+
+	return nonce
+}
+
 // Checks the the tx has enough balance to send a transaction.
 // Returns true if yes, and false if no.
 func (w *Wallet) CheckTxAmount(tx transactions.LuTx) bool {
@@ -59,4 +84,30 @@ func (w *Wallet) CheckTxAmount(tx transactions.LuTx) bool {
 	}
 
 	return true
+}
+
+// This function creates a tx and verifys it.
+// Inputs are the publicKey the tx is going to, and the amount of Luncheon that is being sent.
+// Outputs are the tx, which if empty, means that the amount specified is not possible with your balance.
+func (w *Wallet) CreateTx(toPub string, amount uint64) transactions.LuTx {
+
+	tx := new(transactions.LuTx)
+
+	// Say the tx is from you
+	tx.TxFrom = w.mainKey.GetPubKeyStr()
+
+	tx.TxTo = toPub
+	tx.Value = amount
+
+	tx.Nonce = w.ScanChainForNonce(tx.TxFrom)
+
+	// Simple calculation to get a tx fee
+	tx.Fee = uint64((tx.GetWeight() + 64) * 1000) // The +64 is to add the weight of the signature
+
+	txBytes, _ := json.Marshal(tx)
+
+	_, sig := w.mainKey.SignMsg(txBytes)
+	tx.Signature = hex.EncodeToString(sig)
+
+	return *tx
 }
