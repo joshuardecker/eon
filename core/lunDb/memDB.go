935d989703be5f74851822cc98e3591f269bfb3b
Sucks-To-Suck <jr.plainzainey@gmail.com>
2022-08-28 16:45:27 -0600

    Added new mempool

---
+++ core/lunDb/memDB.go
@@ -0,0 +1,103 @@
+package lundb
+
+import (
+	"errors"
+	"sync"
+)
+
+// A db only stored on the memory.
+// Will be used for storing data that the hard disk doesnt need to store.
+type MemDb struct {
+	db   map[string][]byte // Where bytes are stored
+	lock sync.Mutex        // Will be used to prevent multiple go routines from accessing the db at the same time. For safety.
+}
+
+// Creates and returns a new memDb.
+func InitMemDb() *MemDb {
+
+	return new(MemDb)
+}
+
+// Adds a key and data pair to the mempool, but will not overide if the key is already used.
+// Returns any errors.
+func (db *MemDb) Put(key []byte, data []byte) error {
+
+	// Lock the db so no other go routines can write while this is writting data.
+	db.lock.Lock()
+
+	// Unlock it when done.
+	defer db.lock.Unlock()
+
+	// Is the key already used?
+	if db.has(key) {
+
+		return errors.New("key already in use")
+	}
+
+	db.db[string(key)] = data
+
+	return nil
+}
+
+// Checks if a key already exists.
+// This func is private as it does not use locking and unlocking.
+// This is because only functions that already deal with locking and unlocking use this.
+func (db *MemDb) has(key []byte) bool {
+
+	// Does this key have a value that exists?
+	_, exist := db.db[string(key)]
+
+	return exist
+}
+
+// Checks if a key already exists.
+// This func is public as it is protected with locks.
+func (db *MemDb) Has(key []byte) bool {
+
+	// Lock the db from being read by other go-routines.
+	db.lock.Lock()
+
+	// Unlock it at the end.
+	defer db.lock.Unlock()
+
+	// Does this key have a value that exists?
+	_, exist := db.db[string(key)]
+
+	return exist
+}
+
+// Sets the value of a key and data pair.
+// Ignores if the key is already used or not.
+func (db *MemDb) Set(key []byte, data []byte) error {
+
+	// Lock the db so no other go routines can write while this is writting data.
+	db.lock.Lock()
+
+	// Unlock it when done.
+	defer db.lock.Unlock()
+
+	db.db[string(key)] = data
+
+	return nil
+}
+
+// Deletes an element from the db.
+// Gives an error if the key used does not exist in the db.
+func (db *MemDb) Remove(key []byte) error {
+
+	// Lock the db so no other go routines can write while this is writting data.
+	db.lock.Lock()
+
+	// Unlock it when done.
+	defer db.lock.Unlock()
+
+	// The the db does not have this key.
+	if !db.has(key) {
+
+		return errors.New("cannot delete an element that does not exist")
+	}
+
+	delete(db.db, string(key))
+
+	return nil
+}
