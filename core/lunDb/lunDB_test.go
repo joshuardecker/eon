baaeaf84bfd4d33fe54151170e861d4cee416d70
Sucks-To-Suck <jr.plainzainey@gmail.com>
2022-08-29 11:32:17 -0700

    Added a basic tester for the lunDB

---
+++ core/lunDb/lunDB_test.go
@@ -0,0 +1,24 @@
+package lundb
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestDB(t *testing.T) {
+
+	db := InitDatabase("dbTest")
+
+	// Test the Put meathod.
+	db.Put([]byte("1"), []byte("This is a cool test!"), nil)
+	db.Put([]byte("2"), []byte("Today is the day of daying."), nil)
+	db.Put([]byte("3"), []byte("Mort sends 100 to someone"), nil)
+
+	// Test the Delete meathod.
+	db.Delete([]byte("foo"), nil)
+	db.Delete([]byte("1"), nil)
+
+	// Test the Get meathod.
+	data, _ := db.Get([]byte("2"), nil)
+	fmt.Printf("%s\n", data) // Should print "Today is the day of daying."
+}
