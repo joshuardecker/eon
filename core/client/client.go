0aea8f3f66ddd960f4abb955174a5c48b5157d96
Sucks-To-Suck <jr.plainzainey@gmail.com>
2022-09-15 17:19:46 -0600

    Beginning of restrucutring every type system

---
+++ core/client/client.go
@@ -0,0 +1,8 @@
+package main
+
+// To make a windows copy: GOOS=windows GOARCH=amd64 go build -o luncheon.exe main/main.go
+// Website I got this from: https://freshman.tech/snippets/go/cross-compile-go-programs/
+
+func main() {
+
+}
