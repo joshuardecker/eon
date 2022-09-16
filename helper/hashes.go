21d4f46abe108e1bf6718ed1ba1e9dd82b73c2e6
Sucks-To-Suck <jr.plainzainey@gmail.com>
2022-09-15 21:00:20 -0600

    Added a hash utility library that will be used by much of the program

---
+++ helper/hashes.go
@@ -0,0 +1,135 @@
+package helper
+
+import (
+	"bytes"
+	"encoding/gob"
+	"encoding/hex"
+	"math/big"
+
+	"github.com/Sucks-To-Suck/LuncheonNetwork/logger"
+	"golang.org/x/crypto/sha3"
+)
+
+const HASH_LEN = 32
+
+// Is only a 32 long byte array, but with some fancy functions.
+type hash [HASH_LEN]byte
+
+// ****
+// Value setting / getting:
+
+// Set the value of the hash with bytes.
+func (h *hash) SetBytes(b []byte) {
+
+	if len(b) > 32 {
+
+		// If the bytes are longer than 32, only copy the first 32 bytes
+		b = b[len(b)-HASH_LEN:]
+	}
+
+	copy(h[:], b)
+}
+
+// Set the value of the hash with a string.
+func (h *hash) SetString(s string) {
+
+	h.SetBytes([]byte(s))
+}
+
+// Set the value of the hash with a hex string.
+func (h *hash) SetHex(s string) {
+
+	// Decode the hex.
+	b, _ := hex.DecodeString(s)
+
+	h.SetBytes(b)
+}
+
+// Set the value of the hash with a Big Int.
+func (h *hash) SetBigInt(i *big.Int) {
+
+	h.SetBytes(i.Bytes())
+}
+
+// Get the bytes of the hash.
+func (h *hash) GetBytes() []byte {
+
+	return h[:]
+}
+
+// Get the string of the hash.
+func (h *hash) GetString() string {
+
+	return string(h[:])
+}
+
+// Get the string (hex) of the bytes of the hash.
+func (h *hash) GetHex() string {
+
+	return hex.EncodeToString(h[:])
+}
+
+// Get the Big Int of the bytes of the hash.
+func (h *hash) GetBigInt() *big.Int {
+
+	// Create and assign a value to the big int.
+	i := big.NewInt(0)
+	i.SetBytes(h[:])
+
+	return i
+}
+
+// Value setting / getting:
+// ****
+
+// ****
+// Usage:
+
+// Create and return an emtpy hash.
+func EmptyHash() *hash {
+
+	return new(hash)
+}
+
+// Hash input bytes and return it as a hash type.
+func HashBytes(b []byte) *hash {
+
+	h := new(hash)
+
+	// Hash using shake.
+	sha3.ShakeSum128(h[:], b)
+
+	return h
+}
+
+// This function hashes an inputted interface.
+// Returns nil and logs if an error occurs.
+func HashInterface(val interface{}) *hash {
+
+	h := new(hash)
+
+	// Make a buffer.
+	var buf bytes.Buffer
+
+	// Make an encoder and encode the interface 'i'.
+	encoder := gob.NewEncoder(&buf)
+	err := encoder.Encode(val)
+
+	// If an error occured, log it in red and return nil.
+	if err != nil {
+
+		logger := logger.NewLogger("Helper")
+
+		logger.LogRed("Couldnt hash interface! " + err.Error())
+
+		return nil
+	}
+
+	// Hash the bytes.
+	sha3.ShakeSum128(h[:], buf.Bytes())
+
+	return h
+}
+
+// Usage:
+// ****
---
+++ helper/hashes_test.go
@@ -0,0 +1,53 @@
+package helper
+
+import (
+	"fmt"
+	"math/big"
+	"testing"
+
+	"github.com/Sucks-To-Suck/LuncheonNetwork/logger"
+)
+
+func TestHashes(t *testing.T) {
+
+	// Test value setting:
+
+	logger := logger.NewLogger("Test")
+
+	h := EmptyHash()
+
+	// Test string.
+	h.SetString("This is a test")
+	logger.LogYellow(h.GetString())
+
+	// Test hex.
+	h.SetHex("A13891BFFCD")
+	logger.LogYellow(h.GetHex())
+
+	// Test bytes.
+	b := []byte("This is a test!")
+	fmt.Printf("Bytes: %x\n", b)
+	h.SetBytes(b)
+	fmt.Printf("Bytes: %x\n", h.GetBytes())
+
+	// Test Big Int.
+	// This will return a number much higher than 1, but that is alright.
+	// This is because anything inputted into a Big Int is inputted left to right, not right to left.
+	i := big.NewInt(1)
+	h.SetBigInt(i)
+	logger.LogYellow(h.GetBigInt().String())
+
+	// Test actually hashing:
+
+	// Hash some bytes.
+	fmt.Printf("Hash: %x\n", *HashBytes(b))
+
+	// Hash an interface of any data!
+	fmt.Printf("Hash: %x\n", *HashInterface(
+		[]interface{}{
+			8992,
+			"This is a test",
+			"Sample data",
+			7.98919,
+		}))
+}
