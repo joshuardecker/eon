cabd930b853e05d750104b72c6c417bf90028f8e
Sucks-To-Suck <jr.plainzainey@gmail.com>
2022-09-18 14:27:10 -0600

    Added the gas price type

--- core/gas/gas.go
+++ core/gas/gas.go
@@ -1,5 +1,9 @@
 package gas
 
+import "math/big"
+
 // Gas (measured in Virtual Gas Units) are the fees used on the layer1 blockchain.
 // 1 Vgu = 10^7 Lunch.
 type Gas uint64
+
+type GasPrice *big.Int
