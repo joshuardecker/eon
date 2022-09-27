7ed397cc54d3ffae0fc26773e3044ab2ecd71278
Sucks-To-Suck <jr.plainzainey@gmail.com>
2022-09-27 13:12:12 -0700

    Changed the comments explaining gas and gas price

--- core/gas/gas.go
+++ core/gas/gas.go
@@ -2,8 +2,11 @@ package gas
 
 import "math/big"
 
-// Gas (measured in Virtual Gas Units) are the fees used on the layer1 blockchain.
-// 1 Vgu = 10^7 Lunch.
+// Gas (measured in Virtual Gas Units) tracks data transaction size.
+// This with gas price is used to determine the fees on the layer 1 threader is applicable.
+// 1 Vgu = 1 byte taken up by a data transaction.
 type Gas uint64
 
+// Determines the amount of gas you get per native token.
+// This also means that the highest gas price is 1 token per gas.
 type GasPrice *big.Int
