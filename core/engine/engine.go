9e57b7be618582bff717e9d3bed072f48fb1ecd6
Sucks-To-Suck <jr.plainzainey@gmail.com>
2022-09-27 16:56:56 -0600

    Removed the consensus engine interface

--- core/engine/engine.go
+++
@@ -1,8 +0,0 @@
-package engine
-
-// Here the consensus engine is defined, tieing together the various proof of consensus engines togethe under one interface.
-// To see what the engines are doing, look under the Proof Folders in the main directory.
-// To see how the engine is interacted with, look in threader.
-type ConsensusEngine interface {
-	Verify()
-}
