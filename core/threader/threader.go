1bcb1f72ed5cb0d8b6165249d292a6f0a129f764
Sucks-To-Suck <jr.plainzainey@gmail.com>
2022-10-01 21:23:08 -0600

    Updated the threader to use the config. Updated the AddThread() func to be much smarter

--- core/threader/threader.go
+++ core/threader/threader.go
@@ -3,15 +3,9 @@ package threader
 import (
 	"crypto/ecdsa"
 	"math/big"
+	"sync"
 
-	poa "github.com/Sucks-To-Suck/Eon/PoA"
-	pob "github.com/Sucks-To-Suck/Eon/PoB"
-	pow "github.com/Sucks-To-Suck/Eon/PoW"
 	"github.com/Sucks-To-Suck/Eon/types/thread"
-)
-
-const (
-	MIN_THREADS = 1 // Minimum amount of threads in the Threader.
 )
 
 // The Threader is the mastermind of the blockchains that are threaded together. It covers
@@ -19,43 +13,60 @@ const (
 // threads deployed.
 type Threader struct {
 
-	// The three types of consensus engines that can be used by the threader.
-	// All three are saved that way threads of different types can be verified concurrently.
-	PoAEngine *poa.AuthorityEngine
-	PoBEngine *pob.BurnEngine
-	PoWEngine *pow.WorkEngine
+	// The Proof Engine of the threader can be multiple types, so here we declare it as a interface for flexability.
+	ProofEngine interface{}
 
-	// The threads of the Threader.
+	// The threads of the threader.
 	Threads *[]thread.Thread
 
-	// The chain id of this Threader.
-	Id *big.Int
+	// The length of the threads.
+	// This is saved as a big int, that way we dont need to rely on the len() function to get the length of the threads array.
+	// Relying on the len() function limits the number to an int, making storing it as a Big.Int useless.
+	ThreadsLen *big.Int
 
-	// The public key used by the Threader.
-	Pub *ecdsa.PublicKey
+	// A lock that prevents multiple threads from being made at the same time, which would give them the same ID.
+	ThreadsLock *sync.Mutex
+
+	// The private key used by the threader.
+	Priv *ecdsa.PrivateKey
+
+	// The configuration that will be used by the threader.
+	config *Config
 }
 
-// Creates a new Threader with the given chain id and public key.
-func NewThreader(id big.Int, pub ecdsa.PublicKey) *Threader {
+// Creates a new threader with the given configuration, proofEngine, and the nodes private key.
+func NewThreader(config Config, proofEngine interface{}, private ecdsa.PrivateKey) *Threader {
 
+	// Create the threader.
 	t := new(Threader)
 
-	// Set up the engines.
-	t.PoAEngine = &poa.AuthorityEngine{}
-	t.PoBEngine = &pob.BurnEngine{}
-	t.PoWEngine = &pow.WorkEngine{}
+	// Lock in the proof engine.
+	t.ProofEngine = proofEngine
 
 	// Add a single Thread to start.
-	t.addThread(thread.NewThread(big.NewInt(0)))
+	t.AddThread(thread.NewThread(big.NewInt(0)))
 
-	t.Id = &id
-	t.Pub = &pub
+	t.Priv = &private
+	t.config = &config
 
 	return t
 }
 
-// Simply adds a thread to the Threader.
-func (t *Threader) addThread(th *thread.Thread) {
+// Adds the given thread to the threader. No need to add an ID to the thread before calling this function.
+func (t *Threader) AddThread(th *thread.Thread) {
 
+	// Lock the threads lock while adding a new thread.
+	t.ThreadsLock.Lock()
+
+	// Unlock it when done.
+	defer t.ThreadsLock.Unlock()
+
+	// The id equals the length of the threads array, so if two threads exist, the new thread will have the id of "3".
+	th.Id = t.ThreadsLen
+
+	// Add 1 to the length of the threads.
+	t.ThreadsLen.Add(t.ThreadsLen, big.NewInt(1))
+
+	// Add the new thread to the array.
 	*t.Threads = append(*t.Threads, *th)
 }
