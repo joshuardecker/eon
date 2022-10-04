737a28c978a186170a89bbeae7f04bf58facb7b4
Sucks-To-Suck <jr.plainzainey@gmail.com>
2022-10-03 19:03:50 -0600

    Added the block tester function

---
+++ types/block/block_test.go
@@ -0,0 +1,42 @@
+package block
+
+import (
+	"math/big"
+	"testing"
+
+	"github.com/Sucks-To-Suck/Eon/core/gas"
+	"github.com/Sucks-To-Suck/Eon/eocrypt"
+	"github.com/Sucks-To-Suck/Eon/eocrypt/curve"
+	"github.com/Sucks-To-Suck/Eon/helper"
+)
+
+func TestBlock(t *testing.T) {
+
+	// Header variables:
+	HEAD_HASH := eocrypt.HashBytes([]byte("My cool string!"))
+	private, _ := curve.GenerateKeys()
+	COINBASE := curve.CompressPub(&private.PublicKey)
+	MERKLE := eocrypt.HashBytes([]byte("Merkle"))
+	DIFF := big.NewInt(1)
+	GAS := gas.Gas(1)
+	GAS_MAX := gas.Gas(1)
+	TIME := helper.LocalTime()
+	NONCE := uint(1)
+
+	// The Header created.
+	h := NewHead(*HEAD_HASH, COINBASE, *MERKLE, DIFF, GAS, GAS_MAX, TIME, NONCE)
+
+	// The block created.
+	b := NewBlock(h, nil, nil, TIME)
+
+	// Lets test some functions to see if they break:
+
+	// Get the bytes of the block.
+	if b.Bytes() == nil {
+
+		panic("Cant be nil!")
+	}
+
+	// Print the block.
+	b.Print()
+}
