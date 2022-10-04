85d0bf7b3fe3a9025622147ff9e5817a07cc3183
Sucks-To-Suck <jr.plainzainey@gmail.com>
2022-10-04 16:49:26 -0700

    Added the tx tester, currently failing bc tx still needs an update

---
+++ types/transaction/transaction_test.go
@@ -0,0 +1,32 @@
+package transaction
+
+import (
+	"math/big"
+	"testing"
+
+	"github.com/Sucks-To-Suck/Eon/core/gas"
+	"github.com/Sucks-To-Suck/Eon/eocrypt"
+	"github.com/Sucks-To-Suck/Eon/helper"
+)
+
+func TestTransaction(t *testing.T) {
+
+	tokenHash := eocrypt.HashBytes([]byte("Lunch Coin!"))
+	amount := big.NewInt(1)
+	to := []byte("kaimort123")
+	from := []byte("the-ol-jd")
+	sig := []byte("Totallt Legit Sig")
+	blockFrom := make([]eocrypt.Hash, 1)
+	blockFrom = append(blockFrom, *eocrypt.HashBytes([]byte("100")))
+	txFrom := make([]eocrypt.Hash, 1)
+	txFrom = append(txFrom, *eocrypt.HashBytes([]byte("my-cool-tx")))
+	chain := big.NewInt(100)
+	txGas := gas.Gas(1)
+	gasPrice := gas.GasPrice(big.NewInt(1))
+	txTime := helper.LocalTime()
+
+	tx := NewTransaction(*tokenHash, amount, to, from, sig, blockFrom,
+		txFrom, chain, txGas, gasPrice, txTime)
+
+	tx.Print()
+}
