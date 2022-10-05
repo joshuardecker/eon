33fbf45074a255eb02d905928293455749244b19
Sucks-To-Suck <jr.plainzainey@gmail.com>
2022-10-05 09:45:22 -0700

    Removed Gas Pric type

--- core/gas/gas.go
+++ core/gas/gas.go
@@ -1,7 +1,5 @@
 package gas
 
-import "math/big"
-
 // Gas (measured in Virtual Gas Units) tracks data transaction size.
 // This with gas price is used to determine the fees on the layer 1 threader is applicable.
 // 1 Vgu = 1 byte taken up by a data transaction.
@@ -12,7 +10,3 @@ func (g *Gas) Uint() uint {
 
 	return uint(*g)
 }
-
-// Determines the amount of gas you get per native token.
-// This also means that the highest gas price is 1 token per gas.
-type GasPrice *big.Int
