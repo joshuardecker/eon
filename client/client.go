4d009eb9555e9ca535f8c5918f3c80692c27dc1c
Sucks-To-Suck <jr.plainzainey@gmail.com>
2022-10-07 00:40:03 -0700

    Added this to test something

--- client/client.go
+++ client/client.go
@@ -1,8 +1,11 @@
 package main
 
+import "fmt"
+
 // To make a windows copy: GOOS=windows GOARCH=amd64 go build -o luncheon.exe main/main.go
 // Website I got this from: https://freshman.tech/snippets/go/cross-compile-go-programs/
 
 func main() {
 
+	fmt.Println("Adding this to test something!")
 }
