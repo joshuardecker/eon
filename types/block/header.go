6a5019b432fe48af02c7daf09a7d09356f61f44a
Sucks-To-Suck <jr.plainzainey@gmail.com>
2022-10-10 18:49:10 -0600

    Majorly changed headers to be much more flexable and able to have multiple types (for the different proof types)

--- types/block/header.go
+++ types/block/header.go
@@ -1,84 +1,39 @@
 package block
 
 import (
-	"fmt"
-	"math/big"
 	"time"
 
 	"github.com/Sucks-To-Suck/Eon/core/gas"
 	"github.com/Sucks-To-Suck/Eon/eocrypt"
 )
 
-// A Header defines all of the significant features of a block, such as its parent block, coinbase, gas limit and more.
-type Header struct {
-	parentHash eocrypt.Hash `json:"ParentHash"`
-	coinbase   []byte       `json:"Coinbase"`
-	merkleRoot eocrypt.Hash `json:"Merkle"`
-	difficulty *big.Int     `json:"Diff"`
-	gas        gas.Gas      `json:"GasUsed"`
-	gasLim     gas.Gas      `json:"MaxGas"`
-	time       time.Time    `json:"Time"`
-	nonce      uint         `json:"Nonce"`
-	signature  []byte       `json:"Signature"`
-	threadId   *big.Int     `json:"Thread"`
-}
+// A Header is a flexable type that represents the blocks parameters and essential info.
+// The variation in Headers is because of the variation in Proofs (Authority, Burn, and Work).
+// All of those Proof types have different Header requirements, so a common interface is made to represent them all.
+type Header interface {
 
-// Creates and gives a Head with the given inputs.
-func NewHead(ParentHash eocrypt.Hash, Coinbase []byte, Merkle eocrypt.Hash, Diff *big.Int,
-	Gas gas.Gas, GasLim gas.Gas, Time time.Time, Nonce uint) *Header {
+	// Functions that retrieve information from the Headers:
+	Hash() eocrypt.Hash
 
-	return &Header{
-		parentHash: ParentHash,
-		coinbase:   Coinbase,
-		merkleRoot: Merkle,
-		difficulty: Diff,
-		gas:        Gas,
-		gasLim:     GasLim,
-		time:       Time,
-		nonce:      Nonce,
-	}
-}
+	ParentHash() eocrypt.Hash
 
-// Returns the hash of the Header.
-func (h *Header) Hash() *eocrypt.Hash {
+	Coinbase() []byte
 
-	return eocrypt.HashInterface(
-		[]interface{}{
+	MerkleRoot() eocrypt.Hash
 
-			h.parentHash.GetBytes(),
-			h.coinbase,
-			h.merkleRoot.GetBytes(),
-			h.difficulty.Bytes(),
-			h.gas.Uint(),
-			h.gasLim.Uint(),
-			h.nonce,
-			h.signature,
-		},
-	)
-}
+	Gas() gas.Gas
 
-func (h *Header) ParentHash() eocrypt.Hash { return h.parentHash }
-func (h *Header) Coinbase() []byte         { return h.coinbase }
-func (h *Header) MerkleRoot() eocrypt.Hash { return h.merkleRoot }
-func (h *Header) Difficulty() *big.Int     { return h.difficulty }
-func (h *Header) Gas() gas.Gas             { return h.gas }
-func (h *Header) GasLimit() gas.Gas        { return h.gasLim }
-func (h *Header) Time() time.Time          { return h.time }
-func (h *Header) Nonce() uint              { return h.nonce }
-func (h *Header) Signature() []byte        { return h.signature }
+	GasLimit() gas.Gas
 
-func (h *Header) Print() {
+	Time() time.Time
 
-	fmt.Printf(`
-	[
-		Parent Hash: %x
-		Coinbase: %x
-		Merkle: %x
-		Difficulty: %d
-		Gas: %d
-		Max Gas: %d	
-		Time: %v
-		Nonce: %v
-	]
-	`, h.parentHash, h.coinbase, h.merkleRoot, h.difficulty.Bytes(), h.gas, h.gasLim, h.time, h.nonce)
+	Signature() []byte
+
+	// Functions that modify the in block data:
+	Sign(sig []byte)
+
+	// Misc functions:
+	Print()
+
+	PrintBlock(bHash eocrypt.Hash)
 }
